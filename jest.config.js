1aa563cfda9119893d9d93e4eae418a2caf92cf3
harryli0088 <harryli0088@gmail.com>
2020-12-28 14:38:22 -0500

    reconfig ts and jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.svelte$': 'svelte-jester',
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.ts$': 'babel-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts','js', 'svelte'],
 }
