e5780b9ecaa46cdda75890ba907ebba800706818
harryli0088 <harryli0088@gmail.com>
2020-12-30 16:18:19 -0500

    removed router

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import Router from 'svelte-spa-router'
 	import Home from "Components/Home.svelte"
-
-	const routes = {
-    '/': Home,
-		'*': Home,
-	}
 </script>
 
-<Router {routes}/>
+<main>
+	<Home/>
+</main>
